4ee9f645f768b5c02a02914acaf7a9e416883b75
Deshmore <adesholarmadesi@hotmail.com>
2022-09-21 15:47:35 -0700

    strcat edited 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 
 /**
  *_strcat - concatenates the string pointed to by @src to
- the end of the string pointed to by @dest
- @dest: String that will be appended
- @src: String to be concatenated upon
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
