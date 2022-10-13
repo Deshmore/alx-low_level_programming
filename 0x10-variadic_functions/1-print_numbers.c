ac16aa6ad7409d0ae368862484107207d439df26
Deshmore <adesholarmadesi@hotmail.com>
2022-10-13 15:04:09 -0700

    task one commited

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,11 +16,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 
 	va_start(valist, n);
-	for (i = 0; i , n; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(valist, int);
 		printf("%d", num);
-		if (i , n - 1 && separator)
+		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
